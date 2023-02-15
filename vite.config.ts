4463c27719f4a96cd2f5f42db4524a946a930751
axl <axl@axl>
2023-02-15 20:06:20 +0800

    引入Naive UI配置组件自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+// 配置自动导入插件
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+  ],
+
 })
